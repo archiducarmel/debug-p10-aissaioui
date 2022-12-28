835a74a3014b390750e21b184d8f4130d1de5ba2
Sitou AFANOU <sitou.afanou@sncf.fr>
2022-12-28 10:12:38 +0100

    yet

--- config.py
+++ config.py
@@ -9,8 +9,8 @@ import os
 class DefaultConfig:
     """Configuration for the bot"""
     PORT = 8000
-    APP_ID = "5595c354-9014-4161-bd6b-c1b379992752"
-    APP_PASSWORD = "dzh8Q~8~UUQlQ2ka5PuMmcq2SjchCG1Khc9v1bfo"
+    APP_ID = "c29d82c3-79e2-480f-bb3a-d9fa13e2ebd0"
+    APP_PASSWORD = "HKD8Q~QXP3OWmq_cIIiX-2RlzokHzkRIYq~V9dmP"
     LUIS_APP_ID = "45c98dc5-6ad6-4693-a51f-89d28ffc032a"
     LUIS_API_KEY = "315164affd6148cb9530f673610b291e"
     LUIS_API_HOST_NAME = "https://p10luisapps-authoring.cognitiveservices.azure.com/"
